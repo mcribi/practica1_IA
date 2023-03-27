3b72f199e6db3ded6924ef55fb0a76b0ce128d43
Maria Cribilles <mariacribilled@gmail.com>
2023-03-27 17:05:15 +0200

    mensaje del comit

--- Comportamientos_Jugador/jugador.cpp
+++ Comportamientos_Jugador/jugador.cpp
@@ -36,9 +36,143 @@ Action ComportamientoJugador::think(Sensores sensores){
 
 
 	// Determinar el efecto de la ultima accion enviada
+	int a;
+	switch(last_action){
+		case actFORWARD:
+			switch(current_state.brujula){
+				case norte: current_state.fil--; break;
+				case noreste: current_state.fil--; current_state.col++; break;
+				case este: current_state.col++; break;
+				case sureste: current_state.fil++; current_state.col++; break;
+				case sur: current_state.fil++; break;
+				case suroeste: current_state.fil++; current_state.col--; break;
+				case oeste: current_state.col--; break;
+				case noroeste: current_state.fil--; current_state.col--; break;
+			}
+			break;
+		case actTURN_SL:
+			a = current_state.brujula;
+			a = (a+7) % 8;
+			current_state.brujula = static_cast<Orientacion>(a);
+			break;
+		case actTURN_SR:
+		a = current_state.brujula;
+			a = (a+1) % 8;
+			current_state.brujula = static_cast<Orientacion>(a);
+			break;
+		case actTURN_BL:
+		a = current_state.brujula;
+			a = (a+5) % 8;
+			current_state.brujula = static_cast<Orientacion>(a);
+			break;
+		case actTURN_BR:
+		a = current_state.brujula;
+		a = (a+3) % 8;
+			current_state.brujula = static_cast<Orientacion>(a);
+			break;
+	}
+
+	// 4
+	if(sensores.terreno[0] == 'G' and !bien_situado){
+		current_state.fil = sensores.posF;
+		current_state.col = sensores.posC;
+		current_state.brujula = sensores.sentido;
+		bien_situado = true;
+	}
+
+	if (sensores.reset){ //si se ha activado el reseteo esta mal situado
+		bien_situado=false;
+	}
+
+	if(bien_situado){
+		PonerTerrenoEnMatriz (sensores.terreno, current_state, mapaResultado);
+	}
+	
+	
+	//DISTINGUIMOS CASOS
+	/*if (mirar_terreno('X',sensores.terreno, current_state)>0){	//hemos visto un objeto y vamos a por el 
+		accion=coger_objetos(sensores.terreno, current_state);
+	}else if (mirar_terreno('D',sensores.terreno, current_state)>0)
+	{
+		accion=coger_objetos(sensores.terreno, current_state);
+	}else if (mirar_terreno('K',sensores.terreno, current_state)>0)
+	{
+		accion=coger_objetos(sensores.terreno, current_state);
+	}
+	
+	else */if((sensores.terreno[2] == 'T' or sensores.terreno[2] == 'S' or sensores.terreno[2] == 'G') and sensores.superficie[2] == '_'){
+		accion = actFORWARD;
+	}
+	else if (sensores.terreno[2]=='K' or sensores.terreno[2]=='D' or sensores.terreno[2]=='X'){
+		if (sensores.terreno[2]=='K'){
+    		bikini=true; 
+    		accion = actFORWARD;
+    	}
+		else if (sensores.terreno[2]=='D'){
+		zapatillas=true;
+		accion = actFORWARD; 
+		}
+		else if (sensores.terreno[2]=='X'){
+		recarga=true; 
+		accion = actFORWARD;
+		}
+	}
+	else if (sensores.terreno[2]=='P' or sensores.terreno[2]=='M'){
+		//accion=actTURN_BL;
+		int girar=rand()%4;
+    	if (girar==0){
+			accion=actTURN_SR;
+		}else if (girar==1){
+			accion=actTURN_BL;
+		}else if (girar==2){
+			accion=actTURN_SL;
+		}else if (girar==3){
+			accion=actTURN_BR;
+		}
+	}
+	else if (sensores.terreno[2] == 'B' and zapatillas==true){
+			accion = actFORWARD;	
+	}
+	else if (sensores.terreno[2] == 'A' and bikini==true){
+			accion = actFORWARD;	
+	}
+	else if (sensores.terreno[2] == 'B' or sensores.terreno[2] == 'A'){
+		//accion=actTURN_BR;
+		int giro=rand()%4;
+		if (giro==0){
+			//giro=(giro+1)%4;
+			accion=actTURN_SR;
+		}else if (giro==1){
+			//giro=(giro+1)%4;
+			accion=actTURN_BL;
+		}else if (giro==2){
+			//giro=(giro+1)%4;
+			accion=actTURN_SL;
+		}else if (giro==3){
+			//giro=(giro+1)%4;
+			accion=actTURN_BR;
+		}
+	}
+	
+	last_action = accion;
 	return accion;
 }
 
 int ComportamientoJugador::interact(Action accion, int valor){
   return false;
 }
+
+/*Action giro_random(){
+    Action accion;
+    int girar=rand()%4;
+    if (girar==0){
+			accion=actTURN_SR;
+		}else if (girar==1){
+			accion=actTURN_SR;
+		}else if (girar==2){
+			accion=actTURN_SL;
+		}else if (girar==3){
+			accion=actTURN_SR;
+		}
+    return accion; 
+}*/
--- Comportamientos_Jugador/jugador.hpp
+++ Comportamientos_Jugador/jugador.hpp
@@ -4,12 +4,30 @@
 #include "comportamientos/comportamiento.hpp"
 using namespace std;
 
+struct state{
+  int fil;
+  int col;
+  Orientacion brujula;
+};
+
+
 class ComportamientoJugador : public Comportamiento{
 
   public:
     ComportamientoJugador(unsigned int size) : Comportamiento(size){
       // Constructor de la clase
       // Dar el valor inicial a las variables de estado
+      last_action = actIDLE;
+      //Orientacion = norte;
+      current_state.fil = current_state.col = 99; //valor por defecto
+      current_state.brujula = norte;
+      bien_situado=false;
+      girar_derecha = false;
+      zapatillas=false;
+      recarga=false;
+      ir_objeto=false;
+      girar_izquierda=false;
+      giro=0;
     }
 
     ComportamientoJugador(const ComportamientoJugador & comport) : Comportamiento(comport){}
@@ -21,7 +39,274 @@ class ComportamientoJugador : public Comportamiento{
   private:
   
   // Declarar aquí las variables de estado
+  Action last_action;
+  //Orientacion brujula;
 
+  state current_state;
+
+  bool girar_derecha;
+  bool bien_situado;
+  bool zapatillas;
+  bool bikini;
+  bool recarga;
+  bool ir_objeto;
+  bool girar_izquierda;
+  int giro;
+
+  void PonerTerrenoEnMatriz (const vector<unsigned char> &terreno, const state &st, vector<vector<unsigned char>>&matriz){
+    //Actualiza el mapa con la casilla donde se encuentre el agente
+    matriz[st.fil][st.col] = terreno[0];
+    if ((current_state.brujula==norte) or (current_state.brujula==sur) or (current_state.brujula==este) or (current_state.brujula==oeste))
+      if (current_state.brujula==sur){
+        matriz[st.fil][st.col] = terreno[0];
+
+        matriz[st.fil+1][st.col+1] = terreno[1];
+        matriz[st.fil+1][st.col] = terreno[2];
+        matriz[st.fil+1][st.col-1] = terreno[3];
+
+        matriz[st.fil+2][st.col+2] = terreno[4];
+        matriz[st.fil+2][st.col+1] = terreno[5];
+        matriz[st.fil+2][st.col] = terreno[6];
+        matriz[st.fil+2][st.col-1] = terreno[7];
+        matriz[st.fil+2][st.col-2] = terreno[8];
+
+        matriz[st.fil+3][st.col+3] = terreno[9];
+        matriz[st.fil+3][st.col+2] = terreno[10];
+        matriz[st.fil+3][st.col+1] = terreno[11];
+        matriz[st.fil+3][st.col] = terreno[12];
+        matriz[st.fil+3][st.col-1] = terreno[13];
+        matriz[st.fil+3][st.col-2] = terreno[14];
+        matriz[st.fil+3][st.col-3] = terreno[15];
+        
+        /*for(int i=0; i<4; i++){
+          for (int j=0; j<16; j++){
+            matriz[st.fil+i][st.col] = terreno[j];
+            matriz[st.fil+i][st.col] = terreno[j];
+            matriz[st.fil+i][st.col] = terreno[j];
+          }
+        }*/
+  
+      }
+
+      if (current_state.brujula==norte){
+        matriz[st.fil][st.col] = terreno[0];
+
+        matriz[st.fil-1][st.col-1] = terreno[1];
+        matriz[st.fil-1][st.col] = terreno[2];
+        matriz[st.fil-1][st.col+1] = terreno[3];
+
+        matriz[st.fil-2][st.col-2] = terreno[4];
+        matriz[st.fil-2][st.col-1] = terreno[5];
+        matriz[st.fil-2][st.col] = terreno[6];
+        matriz[st.fil-2][st.col+1] = terreno[7];
+        matriz[st.fil-2][st.col+2] = terreno[8];
+
+        matriz[st.fil-3][st.col-3] = terreno[9];
+        matriz[st.fil-3][st.col-2] = terreno[10];
+        matriz[st.fil-3][st.col-1] = terreno[11];
+        matriz[st.fil-3][st.col] = terreno[12];
+        matriz[st.fil-3][st.col+1] = terreno[13];
+        matriz[st.fil-3][st.col+2] = terreno[14];
+        matriz[st.fil-3][st.col+3] = terreno[15];
+      }
+
+      if (current_state.brujula==este){
+        matriz[st.fil][st.col] = terreno[0];
+
+        matriz[st.fil-1][st.col+1] = terreno[1];
+        matriz[st.fil][st.col+1] = terreno[2];
+        matriz[st.fil+1][st.col+1] = terreno[3];
+
+        matriz[st.fil-2][st.col+2] = terreno[4];
+        matriz[st.fil-1][st.col+2] = terreno[5];
+        matriz[st.fil][st.col+2] = terreno[6];
+        matriz[st.fil+1][st.col+2] = terreno[7];
+        matriz[st.fil+2][st.col+2] = terreno[8];
+
+        matriz[st.fil-3][st.col+3] = terreno[9];
+        matriz[st.fil-2][st.col+3] = terreno[10];
+        matriz[st.fil-1][st.col+3] = terreno[11];
+        matriz[st.fil][st.col+3] = terreno[12];
+        matriz[st.fil+1][st.col+3] = terreno[13];
+        matriz[st.fil+2][st.col+3] = terreno[14];
+        matriz[st.fil+3][st.col+3] = terreno[15];
+      }
+
+      if (current_state.brujula==oeste){
+        matriz[st.fil][st.col] = terreno[0];
+
+        matriz[st.fil+1][st.col-1] = terreno[1];
+        matriz[st.fil][st.col-1] = terreno[2];
+        matriz[st.fil-1][st.col-1] = terreno[3];
+
+        matriz[st.fil+2][st.col-2] = terreno[4];
+        matriz[st.fil+1][st.col-2] = terreno[5];
+        matriz[st.fil][st.col-2] = terreno[6];
+        matriz[st.fil-1][st.col-2] = terreno[7];
+        matriz[st.fil-2][st.col-2] = terreno[8];
+
+        matriz[st.fil+3][st.col-3] = terreno[9];
+        matriz[st.fil+2][st.col-3] = terreno[10];
+        matriz[st.fil+1][st.col-3] = terreno[11];
+        matriz[st.fil][st.col-3] = terreno[12];
+        matriz[st.fil-1][st.col-3] = terreno[13];
+        matriz[st.fil-2][st.col-3] = terreno[14];
+        matriz[st.fil-3][st.col-3] = terreno[15];
+      }
+
+      if (current_state.brujula==noreste){
+        matriz[st.fil][st.col] = terreno[0];
+
+        matriz[st.fil-1][st.col] = terreno[1];
+        matriz[st.fil-1][st.col+1] = terreno[2];
+        matriz[st.fil][st.col+1] = terreno[3];
+
+        matriz[st.fil-2][st.col] = terreno[4];
+        matriz[st.fil-2][st.col+1] = terreno[5];
+        matriz[st.fil-2][st.col+2] = terreno[6];
+        matriz[st.fil-1][st.col+2] = terreno[7];
+        matriz[st.fil][st.col+2] = terreno[8];
+
+        matriz[st.fil-3][st.col] = terreno[9];
+        matriz[st.fil-3][st.col+1] = terreno[10];
+        matriz[st.fil-3][st.col+2] = terreno[11];
+        matriz[st.fil-3][st.col+3] = terreno[12];
+        matriz[st.fil-2][st.col+3] = terreno[13];
+        matriz[st.fil-1][st.col+3] = terreno[14];
+        matriz[st.fil][st.col+3] = terreno[15];
+      }
+
+      if (current_state.brujula==noroeste){
+        matriz[st.fil][st.col] = terreno[0];
+
+        matriz[st.fil][st.col-1] = terreno[1];
+        matriz[st.fil-1][st.col-1] = terreno[2];
+        matriz[st.fil-1][st.col] = terreno[3];
+
+        matriz[st.fil][st.col-2] = terreno[4];
+        matriz[st.fil-1][st.col-2] = terreno[5];
+        matriz[st.fil-2][st.col-2] = terreno[6];
+        matriz[st.fil-2][st.col-1] = terreno[7];
+        matriz[st.fil-2][st.col] = terreno[8];
+
+        matriz[st.fil][st.col-3] = terreno[9];
+        matriz[st.fil-1][st.col-3] = terreno[10];
+        matriz[st.fil-2][st.col-3] = terreno[11];
+        matriz[st.fil-3][st.col-3] = terreno[12];
+        matriz[st.fil-3][st.col-2] = terreno[13];
+        matriz[st.fil-3][st.col-1] = terreno[14];
+        matriz[st.fil-3][st.col] = terreno[15];
+      }
+
+      if (current_state.brujula==suroeste){
+        matriz[st.fil][st.col] = terreno[0];
+
+        matriz[st.fil+1][st.col] = terreno[1];
+        matriz[st.fil+1][st.col-1] = terreno[2];
+        matriz[st.fil][st.col-1] = terreno[3];
+
+        matriz[st.fil+2][st.col] = terreno[4];
+        matriz[st.fil+2][st.col-1] = terreno[5];
+        matriz[st.fil+2][st.col-2] = terreno[6];
+        matriz[st.fil+1][st.col-2] = terreno[7];
+        matriz[st.fil][st.col-2] = terreno[8];
+
+        matriz[st.fil+3][st.col] = terreno[9];
+        matriz[st.fil+3][st.col-1] = terreno[10];
+        matriz[st.fil+3][st.col-2] = terreno[11];
+        matriz[st.fil+3][st.col-3] = terreno[12];
+        matriz[st.fil+2][st.col-3] = terreno[13];
+        matriz[st.fil+1][st.col-3] = terreno[14];
+        matriz[st.fil][st.col-3] = terreno[15];
+      }
+
+      if (current_state.brujula==sureste){
+        matriz[st.fil][st.col] = terreno[0];
+
+        matriz[st.fil][st.col+1] = terreno[1];
+        matriz[st.fil+1][st.col+1] = terreno[2];
+        matriz[st.fil+1][st.col] = terreno[3];
+
+        matriz[st.fil][st.col+2] = terreno[4];
+        matriz[st.fil+1][st.col+2] = terreno[5];
+        matriz[st.fil+2][st.col+2] = terreno[6];
+        matriz[st.fil+2][st.col+1] = terreno[7];
+        matriz[st.fil+2][st.col] = terreno[8];
+
+        matriz[st.fil][st.col+3] = terreno[9];
+        matriz[st.fil+1][st.col+3] = terreno[10];
+        matriz[st.fil+2][st.col+3] = terreno[11];
+        matriz[st.fil+3][st.col+3] = terreno[12];
+        matriz[st.fil+3][st.col+2] = terreno[13];
+        matriz[st.fil+3][st.col+1] = terreno[14];
+        matriz[st.fil+3][st.col] = terreno[15];
+      }
+    s
+  }
+
+  //compruebo las casillas que veo 
+  int mirar_terreno(char casilla, const vector<unsigned char> &terreno, const state &st){
+    bool encontrado=false;
+    int donde=-1;
+    for (int i=2;  i<=16; i++){
+      if (terreno[i]==casilla){
+        encontrado=true;
+        donde=i; 
+      }
+    }
+    return donde;
+  }
+
+  Action coger_objetos(const vector<unsigned char> &terreno, const state &st){
+    Action accion_a_realizar;
+    int donde_zapatillas=mirar_terreno('D',terreno, st);
+    int donde_bikini=mirar_terreno('K',terreno, st);
+    int donde_recarga=mirar_terreno('X',terreno, st);
+
+    if (donde_zapatillas>0 and zapatillas==false){ //miro a ver si hay zapatillas
+      if (terreno[donde_zapatillas]==2 or terreno[donde_zapatillas]==6 or terreno[donde_zapatillas]==12)
+        accion_a_realizar=actFORWARD;
+      if ((terreno[donde_zapatillas]==1 or terreno[donde_zapatillas]==5 or terreno[donde_zapatillas]==11) and (st.brujula==norte or st.brujula==sur or st.brujula==este or st.brujula==oeste) )
+        accion_a_realizar=actTURN_SL; 
+      else
+        accion_a_realizar=actFORWARD; 
+      if ((terreno[donde_zapatillas]==3 or terreno[donde_zapatillas]==7 or terreno[donde_zapatillas]==8) and (st.brujula==norte or st.brujula==sur or st.brujula==este or st.brujula==oeste))
+        accion_a_realizar=actTURN_SR; 
+      else 
+        accion_a_realizar=actFORWARD; 
+    }
+
+    if (donde_bikini>0 and bikini==false){ //miro a ver si hay zapatillas
+      if (terreno[donde_bikini]==2 or terreno[donde_bikini]==6 or terreno[donde_bikini]==12)
+        accion_a_realizar=actFORWARD;
+      if ((terreno[donde_bikini]==1 or terreno[donde_bikini]==5 or terreno[donde_bikini]==11) and (st.brujula==norte or st.brujula==sur or st.brujula==este or st.brujula==oeste) )
+        accion_a_realizar=actTURN_SL; 
+      else
+        accion_a_realizar=actFORWARD; 
+      if ((terreno[donde_bikini]==3 or terreno[donde_bikini]==7 or terreno[donde_bikini]==8) and (st.brujula==norte or st.brujula==sur or st.brujula==este or st.brujula==oeste))
+        accion_a_realizar=actTURN_SR; 
+      else 
+        accion_a_realizar=actFORWARD; 
+      
+    }
+
+    if (donde_recarga>0 and recarga==false){ //miro a ver si hay zapatillas
+      if (terreno[donde_recarga]==2 or terreno[donde_recarga]==6 or terreno[donde_recarga]==12)
+        accion_a_realizar=actFORWARD;
+      if ((terreno[donde_recarga]==1 or terreno[donde_recarga]==5 or terreno[donde_recarga]==11) and (st.brujula==norte or st.brujula==sur or st.brujula==este or st.brujula==oeste) )
+        accion_a_realizar=actTURN_SL; 
+      else
+        accion_a_realizar=actFORWARD; 
+      if ((terreno[donde_recarga]==3 or terreno[donde_recarga]==7 or terreno[donde_recarga]==8) and (st.brujula==norte or st.brujula==sur or st.brujula==este or st.brujula==oeste))
+        accion_a_realizar=actTURN_SR; 
+      else 
+        accion_a_realizar=actFORWARD; 
+      
+    }
+    return accion_a_realizar; 
+  }
+
+  //Action giro_random(){}
 };
 
 #endif
